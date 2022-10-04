322c5c64945025967c99cc112d9dc82114ac6fb3
ZohaibFattani <fattanizohaib@gmail.com>
2022-10-04 15:54:04 -0700

    finished hero elements

--- bank_modern_app/src/components/Hero.jsx
+++ bank_modern_app/src/components/Hero.jsx
@@ -33,6 +33,20 @@ const Hero = () => (
         Our team of experts.....
       </p>
     </div>
+
+    <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <img src={robot} alt="billing" className="w-[100%] h-[100%] relative z-[5]"/>
+
+      <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"></div>
+      <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-4- white__gradient"></div>
+      <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient"></div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+
+      </div>
+    </div>
+
   </section>
 );
 
